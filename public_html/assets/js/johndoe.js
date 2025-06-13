1664f8f0eea208e26d7cc83252fe41e126d07f1f
franco <francosocatoa@gmail.com>
2025-06-12 23:52:54 -0500

    toggle

--- public_html/assets/js/johndoe.js
+++ public_html/assets/js/johndoe.js
@@ -148,3 +148,21 @@ function initMap() {
       ]
     });    
 }
+
+const menusItemsDropdown = document.querySelectorAll('.menu-item-dropdown');
+menusItemsDropdown.forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+        const subMenu = menuItem.querySelector('.sub-menu');
+        const isActive = menuItem.classList.toggle('sub-menu-toggle');
+        if (subMenu){
+          if (isActive) {
+              subMenu.style.height = `${subMenu.scrollHeight + 6}px`;
+              subMenu.style.padding = '0.2rem 0';
+          }
+          else {
+              subMenu.style.height = '0';
+              subMenu.style.padding = '0';
+          }
+        }  
+    });
+});
